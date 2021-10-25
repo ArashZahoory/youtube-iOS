0500ce92b7c2c914f4ec41abf986ef552d4394c3
Arash Zahoory <arashzahoory@Arashs-Mac-mini.local>
2021-10-25 15:40:47 -0700

    completed most of testHomeScreen

--- YouTubeUITests/YouTubeUITests.swift
+++ YouTubeUITests/YouTubeUITests.swift
@@ -25,7 +25,7 @@ class YouTubeUITests: XCTestCase {
 
     
     // The comments seen INSIDE the function are not necessary, but included to assist in the effort of reviewing this assignment
-    func testSearchScenario() {
+        func testSearch() {
         // UI tests must launch the application that they test.\
         let testString = "test"
         let testoutString = "testout"
@@ -39,7 +39,7 @@ class YouTubeUITests: XCTestCase {
         // Type “test” in the search bar.
         let textField = app.textFields["Search on Youtube"]
         textField.typeText(testString)
-
+            
         // Select the option “testout” from the results list.
         let tablesQuery = app.tables
         tablesQuery.staticTexts[testoutString].tap()
@@ -54,18 +54,70 @@ class YouTubeUITests: XCTestCase {
         // there doesn't seem to be a requirement for an assertion here
     }
 
-    
+    //         Settings:
     func testSettings() {
+        // Store Settings Menu items
+        let settingsMenu = ["Settings", "Terms & privacy policy", "Send Feedback", "Help", "Switch Account", "Cancel"]
+        
         let app = XCUIApplication()
         app.launch()
         
+        // Tap on Settings; Check all the options are displayed 
+        app.navigationBars["YouTube"].buttons["navSettings"].tap()
+        for settingsOptions in settingsMenu{
+            //if exists, and is contained
+            XCTAssertTrue(app.staticTexts[settingsOptions].exists)
+            print(settingsOptions + " is true ")
+        }
+        app.staticTexts["Cancel"].tap()
         
+
+        // Tap on Settings; tap on Terms & privacy policy; check if the modal is dismissed.
+        app.navigationBars["YouTube"].buttons["navSettings"].tap()
+        app.staticTexts["Terms & privacy policy"].tap()
+        XCTAssertFalse(app.staticTexts["Cancel"].exists)
+        
+        // Tap on Settings; tap on Send Feedback; check if modal is dismissed.
+        app.navigationBars["YouTube"].buttons["navSettings"].tap()
+        app.staticTexts["Send Feedback"].tap()
+        XCTAssertFalse(app.staticTexts["Cancel"].exists)
+        
+        // Tap on Settings; tap on Switch Account; check if modal is dismissed.
+        app.navigationBars["YouTube"].buttons["navSettings"].tap()
+        app.staticTexts["Switch Account"].tap()
+        XCTAssertFalse(app.staticTexts["Cancel"].exists)
+        
+        // Tap on Settings; tap on Cancel; check if modal is dismissed.
+        app.navigationBars["YouTube"].buttons["navSettings"].tap()
+        app.staticTexts["Cancel"].tap()
+        XCTAssertFalse(app.staticTexts["Cancel"].exists)
     }
     
+    
+        //  Scenarios for Home Screen:
     func testHomeScreen() {
         let app = XCUIApplication()
         app.launch()
         
+        //  Swipe to the 3rd video.
+        app.swipeUp()
         
+        //create array of strings to hold the titles/values
+        var elementLabels = [String]()
+        for i in 0..<app.tables.staticTexts.count {
+            elementLabels.append (app.tables.staticTexts.element(boundBy: i).label)
+        }
+
+        //the third video is always index 23 in the array elementLabels
+        let stringName = elementLabels[23]
+        app.staticTexts[stringName].firstMatch.tap()
+        
+        //  Validate all the elements available on the screen.****
+        
+        //  Tap on the last video on the screen. Check if the UI element is selected. ****
+        
+        //  Minimize the video player.
+        app.buttons["minimize"].tap()
     }
 }
+
